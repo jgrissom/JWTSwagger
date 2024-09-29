bfda7baf55d5059c0ab37d0d16a4f78d596604ad
jgrissom <jgrissom91@gmail.com>
2024-09-28 20:37:35 -0500

    [1.11] User Data Transfer Object

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,7 +12,12 @@ namespace JWTSwagger.Controllers
     private readonly UserManager<ApplicationUser> _userManager = userManager;
 
     [HttpGet]
-    public IActionResult Get() => Ok(_userManager.Users);
+    public IActionResult Get() => Ok(_userManager.Users.Select(u => 
+      new UserDTO { 
+        Id = u.Id, 
+        Email = u.Email, 
+        UserName = u.UserName 
+      }).OrderBy(u => u.UserName));
 
     [HttpPost]
     [Route("register")]
